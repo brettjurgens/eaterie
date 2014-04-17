fcae181d0b9fe28ca4d0c1eef9dc99dca72864a6
Brett Jurgens <brett@brettjurgens.com>
2014-04-17 15:55:31 -0500

    make posts likeable

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   acts_as_commentable
+  acts_as_likeable
   has_one :recipe
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
 
   acts_as_follower
   acts_as_followable
+  acts_as_liker
 
   has_many :posts
 
